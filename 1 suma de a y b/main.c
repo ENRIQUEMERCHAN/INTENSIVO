ead4a28eb573bd52da40709c2083629f48ad489d
ENRIQUEMERCHAN <enrique.merchan@unet.edu.ve>
2016-09-14 16:53:05 -0400

    Add files via upload

---
+++ 1 suma de a y b/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a,b,c;
+	
+	printf(" ingrese a: ");
+	scanf("%d",&a);
+	printf(" ingrese b: ");
+	scanf("%d",&b);
+	
+	c=a+b;
+	
+	printf("\n el resultado es: %d ",c);
+	
+	return 0;
+}
